df0e6a7bf9de1ec88ef443ad0ff69008fefaed47
Jair Carbajal <jair.carbajal91@gmail.com>
2022-05-12 13:55:02 -0700

    working on delete instance method

--- phases/01-implementation.js
+++ phases/01-implementation.js
@@ -9,8 +9,9 @@ class KeyValuePair {
 class HashTable { // get O(1), set O(1), deleteKey O(1)
 
   constructor(numBuckets = 8) {
-    // Initialize your buckets here
-    // Your code here
+    this.count = 0;
+    this.capacity = numBuckets;
+    this.data = new Array(numBuckets).fill(null);
   }
 
   hash(key) {
@@ -30,22 +31,69 @@ class HashTable { // get O(1), set O(1), deleteKey O(1)
 
 
   insert(key, value) {
-    // Your code here
+    if (this.count / this.capacity > .7) {
+      this.resize();
+    }
+    const index = this.hashMod(key);
+    let curr = this.data[index];
+
+    while(curr && curr.key !== key) {
+      curr = curr.next;
+    }
+    if (curr) {
+      curr.value = value;
+    } else {
+      const pear = new KeyValuePair(key, value);
+      if (this.data[index]) pear.next = this.data[index];
+      this.data[index] = pear;
+      this.count++
+    }
   }
 
 
   read(key) {
-    // Your code here
+    const index = this.hashMod(key);
+    let curr = this.data[index];
+    while(curr && curr.key !== key) {
+      curr = curr.next;
+    }
+    if (!curr) return;
+    return curr.value
   }
 
 
   resize() {
-    // Your code here
+    // hash table property changes should occur first:
+    // copy data to preserve old elements
+    let oldData = this.data;
+    // reassign capacity to double its previous value
+    this.capacity *= 2;
+    // re-instantiate data to an array with its new size filled with null
+    this.data = new Array(this.capacity).fill(null);
+    // reset count (calling insert will re-increment count)
+    this.count = 0;
+
+  // iterate over old data
+    for (let i = 0; i < oldData.length; i++) {
+      // iterate over each element in old data, looking for nested nodes
+      let curr = oldData[i];
+
+      while (curr) {
+        this.insert(curr.key, curr.value);
+        curr = curr.next;
+      }
+      // insert every node back into our new data buckets
+    }
   }
 
 
   delete(key) {
-    // Your code here
+    const index = this.hashMod(key);
+    let curr = this.data[index];
+    if (!curr) return "Key not found";
+    curr = undefined;
+
+    this.count--;
   }
 }
 
